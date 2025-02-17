b7dce04505deac335f0881347b02895e2f28ed6b
boss <smithsaruni16@gmail.com>
2025-02-17 11:17:16 +0300

    neon

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    datasourceUrl: process.env.DATABASE_URL, // Ensures Neon connection works
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
 export default prisma;
 
 // // /lib/prisma.ts
